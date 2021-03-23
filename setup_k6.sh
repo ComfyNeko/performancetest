be2d51940bc50f1899598ec0ebc0405fdc62b608
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-23 22:19:17 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -3,7 +3,7 @@ set -ex
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
+
 echo 'update over'
 sudo apt-get install k6
 echo 'installation complete'
