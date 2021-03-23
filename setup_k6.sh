ca06c2f7d63ba9fabaa999f2b0e4ceaf96a2572c
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-23 22:05:00 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -2,6 +2,7 @@
 set -ex
 
 sudo apt-get update
+echo "update over"
 sudo apt-get install dirmngr --install-recommends
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 
