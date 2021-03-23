357977b133a45bc267e1eff86372bbfa27420b07
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-23 21:53:00 +0530

    Update performance-test.js

--- loadtests/performance-test.js
+++ loadtests/performance-test.js
@@ -4,9 +4,9 @@ import http from "k6/http";
 export let options = {
   duration: "0.30m",
   vus: 50,
-  //thresholds: {
-   // http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
-  //}
+  thresholds: {
+   http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
+  }
 };
 
 export default function() {
