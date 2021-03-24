232760375eeb4d957fcd2498e96456fed144dc36
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-24 11:28:08 +0530

    Create loadtest.js

---
+++ cloud-example/loadtest.js
@@ -0,0 +1,13 @@
+import { sleep } from"k6";
+import http from "k6/http";
+
+export let options = {
+  duration: "0.30m",
+  vus: 50,
+ 
+};
+
+export default function() {
+  http.get('http://35.235.127.18');
+  sleep(3);
+};
