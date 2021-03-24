2d9a59c1bfbc8f2ba1f745f667dd26ccf23a8c41
ComfyNeko <80762247+ComfyNeko@users.noreply.github.com>
2021-03-24 11:34:25 +0530

    Update loadtest.js

--- cloud-example/loadtest.js
+++ cloud-example/loadtest.js
@@ -8,6 +8,8 @@ export let options = {
 };
 
 export default function() {
+  var url = 'http://test.k6.io/login';
+  var payload = JSON.stringify({ email: 'keerthanasrikumar3@gmail.com', password: 'Skeer1209*' });
   http.get('http://35.235.127.18');
   sleep(3);
 };
